5a8c1a0989e906d23d5f9d3f9af085773ffb75d6
Jake Nead <jakenead@gmail.com>
2024-07-05 18:44:10 -0700

    Set up basic two route functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const messages = [
+  {
+    text: "I'll show you the wrong side of a knuckle!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "You look like a flat soda, pal.",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+const express = require("express");
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+/* GET new message page. */
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "New Message" });
+});
+
+router.post("/new", function (req, res, next) {
+  const message = req.body.message;
+  const user = req.body.user;
+  messages.push({ text: message, user: user, added: new Date() });
+  res.redirect("/");
 });
 
 module.exports = router;
