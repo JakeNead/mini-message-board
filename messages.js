345bf1b956f82cf2a87201de7140fa3bc5f3d6c8
Jake Nead <jakenead@gmail.com>
2024-07-27 11:38:52 -0700

    Add sample messages

---
+++ messages.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
